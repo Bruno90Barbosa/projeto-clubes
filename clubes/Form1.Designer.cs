8ef4af2d342b5b5b60c3520c2c043119c4b6dcb8
BrunoBarbosa90 <brunobarbosa90@hotmail.com>
2025-09-21 15:55:37 -0300

    Primeiro commit do projeto

---
+++ clubes/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace clubes
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.pbLogoClube = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogoClube)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Location = new System.Drawing.Point(12, 235);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(139, 41);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Real Madrid";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button2.FlatAppearance.BorderSize = 0;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Location = new System.Drawing.Point(157, 235);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(139, 41);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Manchester United";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button3.FlatAppearance.BorderSize = 0;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Location = new System.Drawing.Point(302, 235);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(139, 41);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Bayern München";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button4.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button4.FlatAppearance.BorderSize = 0;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Location = new System.Drawing.Point(12, 282);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(139, 41);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "Barcelona";
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button5.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button5.FlatAppearance.BorderSize = 0;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.Location = new System.Drawing.Point(157, 282);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(139, 41);
+            this.button5.TabIndex = 4;
+            this.button5.Text = "Liverpool";
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button6.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button6.Location = new System.Drawing.Point(302, 282);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(139, 41);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Milan";
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button7.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button7.FlatAppearance.BorderSize = 0;
+            this.button7.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button7.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button7.Location = new System.Drawing.Point(12, 329);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(139, 41);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Ajax";
+            this.button7.UseVisualStyleBackColor = false;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button8.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button8.FlatAppearance.BorderSize = 0;
+            this.button8.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button8.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button8.Location = new System.Drawing.Point(157, 329);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(139, 41);
+            this.button8.TabIndex = 7;
+            this.button8.Text = "Juventus";
+            this.button8.UseVisualStyleBackColor = false;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button9.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button9.FlatAppearance.BorderSize = 0;
+            this.button9.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button9.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button9.Location = new System.Drawing.Point(302, 329);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(139, 41);
+            this.button9.TabIndex = 8;
+            this.button9.Text = "Porto";
+            this.button9.UseVisualStyleBackColor = false;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button10
+            // 
+            this.button10.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button10.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button10.FlatAppearance.BorderSize = 0;
+            this.button10.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button10.Location = new System.Drawing.Point(12, 376);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(139, 41);
+            this.button10.TabIndex = 9;
+            this.button10.Text = "Borussia Dortmund";
+            this.button10.UseVisualStyleBackColor = false;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button11.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button11.FlatAppearance.BorderSize = 0;
+            this.button11.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button11.Location = new System.Drawing.Point(157, 376);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(139, 41);
+            this.button11.TabIndex = 10;
+            this.button11.Text = "Inter de Milão";
+            this.button11.UseVisualStyleBackColor = false;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.button12.FlatAppearance.BorderColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.button12.FlatAppearance.BorderSize = 0;
+            this.button12.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button12.Location = new System.Drawing.Point(302, 376);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(139, 41);
+            this.button12.TabIndex = 11;
+            this.button12.Text = "Arsenal";
+            this.button12.UseVisualStyleBackColor = false;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // pbLogoClube
+            // 
+            this.pbLogoClube.BackColor = System.Drawing.SystemColors.Control;
+            this.pbLogoClube.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pbLogoClube.Location = new System.Drawing.Point(12, 12);
+            this.pbLogoClube.Name = "pbLogoClube";
+            this.pbLogoClube.Size = new System.Drawing.Size(216, 217);
+            this.pbLogoClube.TabIndex = 12;
+            this.pbLogoClube.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(265, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(144, 30);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Clique no nome do clube\r\ne veja o logo";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gainsboro;
+            this.ClientSize = new System.Drawing.Size(453, 425);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pbLogoClube);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button9);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Logos Clubes de Futebol";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbLogoClube)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.PictureBox pbLogoClube;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ clubes/Form1.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace clubes
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            pbLogoClube.SizeMode = PictureBoxSizeMode.Zoom;
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            pbLogoClube.Image = Image.FromFile("imagens\\Real_Madrid.png");
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            pbLogoClube.Image = Image.FromFile("imagens\\Manchester_United_FC_logo.png");
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            pbLogoClube.Image = Image.FromFile("imagens\\FC_Bayern_München_logo_(2017).svg.png");
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            pbLogoClube.Image = Image.FromFile("imagens\\barcelona-4096.png");
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            pbLogoClube.Image = Image.FromFile("imagens\\Liverpool_FC.svg.png");
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            pbLogoClube.Image = Image.FromFile("imagens\\Logo_of_AC_Milan.svg.png");
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            pbLogoClube.Image = Image.FromFile("imagens\\Ajax_Amsterdam.svg.png");
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            pbLogoClube.Image = Image.FromFile("imagens\\Juventus_FC_2017_logo.png");
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            pbLogoClube.Image = Image.FromFile("imagens\\logo-porto-2048.png");
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            pbLogoClube.Image = Image.FromFile("imagens\\Borussia_Dortmund_logo.svg.png");
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            pbLogoClube.Image = Image.FromFile("imagens\\Inter_Milano_2021_logo_with_2_stars.svg.png");
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            pbLogoClube.Image = Image.FromFile("imagens\\Arsenal_FC.svg.png");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        
+    }
+}
